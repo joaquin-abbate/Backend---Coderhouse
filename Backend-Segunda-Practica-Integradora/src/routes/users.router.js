a1c68a73238dec172fb2dc090f2b1cb4a4ebfb93
Joaquin Abbate <joaquinabbate79@gmail.com>
2023-07-06 18:14:38 -0300

    Subida-Final

--- Backend-Segunda-Practica-Integradora/src/routes/users.router.js
+++ Backend-Segunda-Practica-Integradora/src/routes/users.router.js
@@ -1,13 +1,24 @@
-import { Router } from 'express'
-import { register, login, loginFront, privateRoute } from '../controllers/user.controller.js';
-import { checkAuth } from '../jwt/auth.js';
+import { Router } from "express";
+import {
+  register,
+  login,
+  loginFront,
+  privateRoute,
+} from "../controllers/user.controller.js";
+import { checkAuth } from "../jwt/auth.js";
+import passport from "passport";
 
-const router = Router()
+const router = Router();
 
-router.post('/register', register);
+router.post("/register", register);
 
-router.get('/private', checkAuth, privateRoute);
+router.post("/login", login);
 
-router.post('/loginfront', loginFront);
+router.get("/private", checkAuth, privateRoute);
 
+router.post("/loginfront", loginFront);
+
+router.get("/private2", passport.authenticate("jwt"), (req, res) => {
+  res.send(req.user);
+});
 export default router;
--- Backend-Segunda-Practica-Integradora/src/server.js
+++ Backend-Segunda-Practica-Integradora/src/server.js
@@ -1,26 +1,54 @@
-import express from 'express'
-import { __dirname } from './utils.js'
-import usersRouter from './routes/users.router.js'
-import handlebars from 'express-handlebars'
-import viewsRouter from './routes/views.routes.js'
-import './db/dbConfig.js'
-import { errorHandler } from './middlewares/errorHandler.js'
+import express from "express";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { __dirname } from "./utils.js";
+import usersRouter from "./routes/users.router.js";
+import "./db/dbConfig.js";
+import mongoStore from "connect-mongo";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import passport from "passport";
+import viewsRouter from "./routes/views.routes.js";
+import handlebars from "express-handlebars";
+import "./passport/jwt.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
- 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "sessionKey",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 10000,
+    },
+    store: new mongoStore({
+      mongoUrl:
+        "mongodb+srv://admin:admin@cluster0.vcjyxe3.mongodb.net/coderhouse?retryWrites=true&w=majority",
+      //autoRemove: "interval",
+      ttl: 10,
+      // crypto: {
+      //   secret: '1234',       //encripta los datos de la sesion
+      // },
+    }),
+  })
+);
+
 app.use(errorHandler);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(express.static(__dirname+'/public'))
-app.engine('handlebars',handlebars.engine())
-app.set('views',__dirname+'/views')
-app.set('view engine', 'handlebars')
+app.use("/users", usersRouter);
+app.use(express.static(__dirname + "/public"));
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
 
-app.use('/users',usersRouter)
-app.use('/', viewsRouter)
+app.use("/", viewsRouter);
 
-const PORT = 8080
+const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`Escuchando al puerto ${PORT}`)
-})
+  console.log(`Escuchando al puerto ${PORT}`);
+});
